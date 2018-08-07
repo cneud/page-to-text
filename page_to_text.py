69676b3486c166ebdb6eba8d0ae22f9e75137e08
Clemens Neudecker <clemens.neudecker@gmail.com>
2018-08-07 18:02:40 +0200

    support newer PAGE schema versions

--- page_to_text.py
+++ page_to_text.py
@@ -12,7 +12,10 @@ if sys.stdout.encoding != 'UTF-8':
     namespace = {'page_2009-03-16': 'http://schema.primaresearch.org/PAGE/gts/pagecontent/2009-03-16',
                  'page_2010-01-12': 'http://schema.primaresearch.org/PAGE/gts/pagecontent/2010-01-12',
                  'page_2010-03-19': 'http://schema.primaresearch.org/PAGE/gts/pagecontent/2010-03-19',
-                 'page_2013-07-15': 'http://schema.primaresearch.org/PAGE/gts/pagecontent/2013-07-15'}
+                 'page_2013-07-15': 'http://schema.primaresearch.org/PAGE/gts/pagecontent/2013-07-15',
+                 'page_2016-07-15': 'http://schema.primaresearch.org/PAGE/gts/pagecontent/2016-07-15',
+                 'page_2017-07-15': 'http://schema.primaresearch.org/PAGE/gts/pagecontent/2017-07-15',
+                 'page_2018-07-15': 'http://schema.primaresearch.org/PAGE/gts/pagecontent/2018-07-15'}
     tree = ET.parse(sys.argv[1])
     xmlns = tree.getroot().tag.split('}')[0].strip('{')
     if xmlns in namespace.values():
